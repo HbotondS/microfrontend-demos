01b4f1bef33e951fa73b1922b1f2adea3fdff805
Botond Sámuel Hegyi <36891185+HbotondS@users.noreply.github.com>
2024-02-01 08:55:30 +0000

    fixed typo in devcontainer

--- .devcontainer/startUp.sh
+++ .devcontainer/startUp.sh
@@ -7,4 +7,5 @@ curl -fsSL https://bun.sh/install | bash
 export CODESPACE_NAME=$(gh codespace view | head -n 1 | sed 's/Name[[:space:]]*//')
 
 # Create webpack config file
-python webpack-config.py
+# fixme: coomenting out python script until it's fixed
+# python webpack-config.py
