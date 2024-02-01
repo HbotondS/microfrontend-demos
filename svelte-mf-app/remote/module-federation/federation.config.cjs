f1f7a3e2647c873df275d6b465ffffcf57253000
Botond Sámuel Hegyi <36891185+HbotondS@users.noreply.github.com>
2024-02-01 18:22:50 +0000

    added svelte remote app

---
+++ svelte-mf-app/remote/module-federation/federation.config.cjs
@@ -0,0 +1,20 @@
+const {
+  withNativeFederation,
+  shareAll,
+} = require("@softarc/native-federation/build");
+
+module.exports = withNativeFederation({
+  name: "remote",
+  exposes: {
+    "./Counter": "./src/Counter.svelte"
+  },
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+      includeSecondaries: false,
+    }),
+  },
+
+});
---
+++ svelte-mf-app/remote/vite.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig, loadEnv } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { federation } from '@module-federation/vite';
+import { createEsBuildAdapter } from '@softarc/native-federation-esbuild';
+import { sveltePlugin } from './module-federation/esbuild-svelte-plugin';
+import { writeFileSync, existsSync, mkdirSync } from "fs";
+
+export default defineConfig(async ({ command, mode }) => {
+	const selfEnv = loadEnv(mode, process.cwd());
+	return {
+		plugins: [
+			{
+			name: "generate-enviroment",
+			options: function () {
+				console.info("selfEnv", selfEnv);
+				if (!existsSync("dist")){
+			    mkdirSync("dist");
+				}
+				writeFileSync(
+					"./dist/remoteEntry.json",
+					`{}`
+				);
+			},
+			},
+			await federation({
+				options: {
+					workspaceRoot: __dirname,
+					outputPath: 'dist',
+					tsConfig: 'tsconfig.json',
+					federationConfig: 'module-federation/federation.config.cjs',
+					verbose: false,
+					dev: command === 'serve',
+				},
+				adapter: createEsBuildAdapter({
+					plugins: [sveltePlugin],
+				}),
+			}),
+			svelte(),
+		],
+	};
+});
