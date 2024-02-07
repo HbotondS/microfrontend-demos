177e0896d5c8199d86c1094b4c21af7f77709b98
HbotondS <hbotond97@gmail.com>
2024-02-07 20:05:45 +0200

    testing eslint with svelte

--- svelte-mf-app/remote/src/Counter.svelte
+++ svelte-mf-app/remote/src/Counter.svelte
@@ -7,6 +7,6 @@
 </script>
 
 <div class="w-fit border-2 border-red-500 border-dashed rounded-lg p-2">
-  <div>count = {count}</div>
+  <div>count = { count }</div>
   <button class="rounded-full bg-green-500 text-white p-2" on:click={increment}>add one</button>
 </div>
