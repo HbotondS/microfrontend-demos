3ee7b4ea44ebaeebbc02cc0ef87479fdd05b32ce
Botond Sámuel Hegyi <36891185+HbotondS@users.noreply.github.com>
2024-03-05 17:55:54 +0000

    added remote app for svelte-mf-app with originjs plugin

---
+++ svelte-mf-app/originjs-plugin/remote/src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<div class="mt-10 text-3xl mx-auto max-w-6xl">
+  <div>Name: remote</div>
+  <Counter />
+</div>
---
+++ svelte-mf-app/originjs-plugin/remote/src/lib/Counter.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  let count: number = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<div class="w-fit border-2 border-red-500 border-dashed rounded-lg p-2">
+  <div>count = {count}</div>
+  <button class="rounded-full bg-green-500 text-white p-2" on:click={increment}>add one</button>
+</div>
