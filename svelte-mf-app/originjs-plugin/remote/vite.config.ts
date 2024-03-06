ec523989f488e1aa3e9f44810fc679ff2ef7cb27
Botond Sámuel Hegyi <36891185+HbotondS@users.noreply.github.com>
2024-03-06 08:04:02 +0000

    added and configured vite plugin for module federation

--- svelte-mf-app/originjs-plugin/remote/vite.config.ts
+++ svelte-mf-app/originjs-plugin/remote/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    federation({
+      name: 'remote',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Counter': './src/lib/Counter.svelte',
+      },
+      shared: ['svelte', 'tailwindcss']
+    })
+  ],
 })
