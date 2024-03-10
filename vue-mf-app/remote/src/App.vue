5eba06ff71141570ad64d4816430fe447fd4ac77
HbotondS <hbotond97@gmail.com>
2024-03-10 18:45:53 +0200

    Added vue remote app

---
+++ vue-mf-app/remote/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Counter from './components/Counter.vue'
+</script>
+
+<template>
+  <div class="mt-10 text-3xl mx-auto max-w-6xl">
+    <div>Name: remote</div>
+    <Counter />
+  </div>
+</template>
