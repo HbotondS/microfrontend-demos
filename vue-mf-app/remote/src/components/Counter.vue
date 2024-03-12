509ae87d99c6fc17df5eb4be91b335aa42a2fa0a
Botond Sámuel Hegyi <36891185+HbotondS@users.noreply.github.com>
2024-03-12 18:59:47 +0000

    updated counter method

--- vue-mf-app/remote/src/components/Counter.vue
+++ vue-mf-app/remote/src/components/Counter.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
+  import { storeToRefs } from "pinia"
+  import { useStore } from "../stores/counter"
 
-  const count = ref(0)
-
-  const increment = () => {
-    count.value++
-  }
+  const store = useStore()
+  const { count } = storeToRefs(store)
 </script>
 
 <template>
   <div class="w-fit border-2 border-red-500 border-dashed rounded-lg p-2">
     <div>count = {{ count }}</div>
-    <button class="rounded-full bg-green-500 text-white p-2" @click="increment()">add one</button>
+    <button class="rounded-full bg-green-500 text-white p-2" @click="store.increment">add one</button>
   </div>
 </template>
 
