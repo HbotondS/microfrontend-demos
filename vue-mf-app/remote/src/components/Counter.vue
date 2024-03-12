0ab23ed2df5a84c74876cd5cb9f3ba1d2d711ac8
Botond Sámuel Hegyi <36891185+HbotondS@users.noreply.github.com>
2024-03-12 19:15:48 +0000

    Revert "updated counter method"
    This reverts commit 509ae87d99c6fc17df5eb4be91b335aa42a2fa0a.

--- vue-mf-app/remote/src/components/Counter.vue
+++ vue-mf-app/remote/src/components/Counter.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
-  import { storeToRefs } from "pinia"
-  import { useStore } from "../stores/counter"
+  import { ref } from 'vue'
 
-  const store = useStore()
-  const { count } = storeToRefs(store)
+  const count = ref(0)
+
+  const increment = () => {
+    count.value++
+  }
 </script>
 
 <template>
   <div class="w-fit border-2 border-red-500 border-dashed rounded-lg p-2">
     <div>count = {{ count }}</div>
-    <button class="rounded-full bg-green-500 text-white p-2" @click="store.increment">add one</button>
+    <button class="rounded-full bg-green-500 text-white p-2" @click="increment()">add one</button>
   </div>
 </template>
 
